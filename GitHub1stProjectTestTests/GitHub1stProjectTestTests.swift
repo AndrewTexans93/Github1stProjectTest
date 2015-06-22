961b5e881338fbf1c047ab9d9ba518fe88421dca
AndrewTexans93 <Andrewbrook93@gmail.com>
2015-06-22 16:08:32 -0500

    Initial Commit

---
+++ GitHub1stProjectTestTests/GitHub1stProjectTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  GitHub1stProjectTestTests.swift
+//  GitHub1stProjectTestTests
+//
+//  Created by Anita Lee on 6/22/15.
+//  Copyright (c) 2015 Andrew Lee. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GitHub1stProjectTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
