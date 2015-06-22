0f3b3f3d91575a2bef6396d54bdc9a7939a17ac2
AndrewTexans93 <Andrewbrook93@gmail.com>
2015-06-22 16:29:21 -0500

    Initial Commit

---
+++ GitHub1stProjectTest/ViewController2.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController2.swift
+//  GitHub1stProjectTest
+//
+//  Created by Anita Lee on 6/22/15.
+//  Copyright (c) 2015 Andrew Lee. All rights reserved.
+//
+
+import Foundation
+
+
